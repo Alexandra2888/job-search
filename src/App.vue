9259dae9e5f6f0a468b4e86e47c5fece4b828dba
AlexandraM <76844097+Alexandra2888@users.noreply.github.com>
2023-12-06 14:48:30 +0200

    adding MainNav

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,16 @@
-<script setup>
+<script >
+import MainNav from "@/components/nav/mainNav.vue";
 
+export default {
+    name: "App",
+    components: {
+        MainNav
+    }
+}
 </script>
 
 <template>
- 
+ <main-nav/>
 </template>
 
-<style scoped>
 
-</style>
---
+++ src/components/nav/MainNav.vue
@@ -0,0 +1,22 @@
+<template>
+  <header class="w-full text-sm">
+    <div class="fixed left-0 top-0 h-16 w-full bg-white">
+      <div
+        class="mx-auto flex h-full flex-nowrap border-b border-solid border-brand-gray-1 px-8"
+      >
+        <a href="/" class="flex h-full items-center text-xl text-black">{{ company }}</a>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "MainNav",
+  data() {
+    return {
+      company: "New Careers",
+    };
+  },
+};
+</script>
